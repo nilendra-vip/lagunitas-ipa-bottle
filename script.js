c29167261c40beb9b3f5bfe4ed5069a37c5d0a73
rockypatel2613 <patelnilendra809@gmail.com>
2022-10-06 15:48:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+gsap.to("#bottle",{
+  scrollTrigger: {
+    trigger: "#bottle",
+    scroller: "body",
+    start:"top 5%",
+    end:"top -330%",
+    pin:true,
+    scrub: 1,
+    // markers: true
+},
+rotate: -15,
+stagger: 0.5,
+})
+
+gsap.to("#bottle",{
+  scrollTrigger: {
+    trigger: "#bottle",
+    scroller: "body",
+    start:"top 4.8%",
+    end:"top -56%",
+    scrub: 1.2,
+    pin:true,
+    // markers: true
+},  
+scale: .7,
+stagger: 0.5,
+},)
+
